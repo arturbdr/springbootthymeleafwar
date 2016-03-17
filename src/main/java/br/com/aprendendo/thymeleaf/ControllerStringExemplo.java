d23e49c53fae05dfc714f15d4bcb9304e564d70c
Artur Drummond <arturbdr@gmail.com>
2016-03-17 12:49:21 -0300

    da classe no nome

---
+++ src/main/java/br/com/aprendendo/thymeleaf/ControllerStringExemplo.java
@@ -0,0 +1,23 @@
+package br.com.aprendendo.thymeleaf;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Para testar: 
+ *	http://localhost:8080/SpringBootThymeleafWar/ola/artur
+ */
+@Controller
+public class ControllerStringExemplo {
+	
+	@RequestMapping("/ola/{nome}")
+	public @ResponseBody String olaThymeleaf(Map model, @PathVariable("nome") String nome) {
+		return "Ola " + nome;
+		
+	}
+
+}
